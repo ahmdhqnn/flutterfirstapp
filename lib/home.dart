8612b176cd1fe1bb4516d35da01877f6c76108bc
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-07-10 08:25:25 +0700

    crossaxisalignment, textfield, prefixicon

--- lib/home.dart
+++ lib/home.dart
@@ -8,18 +8,18 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        color: Color(0xff51A8FF),
+        color: const Color(0xff51A8FF),
         width: double.infinity,
         height: 350,
-        child: Column(
+        padding: const EdgeInsets.all(16),
+        child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 30),
             Text(
               'Hi Madmax',
-              style: TextStyle(
-                fontSize: 18,
-                color: Color(0xffFFFFFF),
-              ),
+              style: TextStyle(fontSize: 18, color: Color(0xffFFFFFF)),
             ),
             Text(
               "Let's find\nYour Top doctor",
@@ -28,7 +28,21 @@ class HomePage extends StatelessWidget {
                 color: Color(0xffFFFFFF),
                 fontWeight: FontWeight.w600,
               ),
-            )
+            ),
+            SizedBox(height: 30),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: Icon(Icons.search, color: Colors.black, size: 25),
+                hintText: "Search here...",
+                hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Poppins',
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
